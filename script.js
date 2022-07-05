95f24700682c206f2eac301162bb78b54a678bd4
«viktoriya <viki.lar@yandex.ru>
2022-07-05 13:35:21 +0400

    метод XMLHttpRequest

--- script.js
+++ script.js
@@ -13,18 +13,38 @@ const getData = () => {
 getData();
 
 sendData = (user) => {
-  fetch("https://jsonplaceholder.typicode.com/posts", {
-    method: "POST",
-    body: JSON.stringify(user),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.log("ошибка при отправке");
-    });
+  let xhr = new XMLHttpRequest();
+
+  // отслеживаем процесс отправки
+  xhr.upload.onprogress = function (event) {
+    console.log(`Отправлено ${event.loaded} из ${event.total}`);
+  };
+
+  // Ждём завершения: неважно, успешного или нет
+  xhr.onloadend = function () {
+    if (xhr.status == 200 || xhr.status == 201) {
+      console.log("Успех");
+      console.log(user);
+    } else {
+      console.log("Ошибка " + this.status);
+    }
+  };
+
+  xhr.open("POST", "https://jsonplaceholder.typicode.com/posts");
+  xhr.send(JSON.stringify(user));
+
+  // fetch("https://jsonplaceholder.typicode.com/posts", {
+  //   method: "POST",
+  //   body: JSON.stringify(user),
+  //   headers: {
+  //     "Content-type": "application/json; charset=UTF-8",
+  //   },
+  // })
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     console.log(data);
+  //   })
+  //   .catch((error) => {
+  //     console.log("ошибка при отправке");
+  //   });
 };
